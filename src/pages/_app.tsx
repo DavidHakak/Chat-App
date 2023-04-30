1edaeb96dff51da7e9d7972cea0d035e8d43fcda
David Hakak <davidhakak@h-MacBook-Pro-sl-David.local>
2023-04-30 22:32:01 +0300

    base apollo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,18 @@ import {SessionProvider} from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../chakra/theme'
+import { ApolloProvider } from '@apollo/client'
+import { client } from '../graphql/apollo-client'
 
 export default function App({ Component, pageProps: {session , ...pageProps} }: AppProps) {
   console.log("session : "+ JSON.stringify(session) +"\n page props : " + JSON.stringify(pageProps)  )
   return(
+    <ApolloProvider client={client}>
       <SessionProvider session={session}>
          <ChakraProvider theme={theme}>
              <Component {...pageProps} />
          </ChakraProvider>
       </SessionProvider>
+    </ApolloProvider>
   )
 };
